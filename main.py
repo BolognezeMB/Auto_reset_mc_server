9de0d6282f180fbe9dc60404d440d28a040c3c2e
BolognezeMB <bolognieze@gmail.com>
2026-06-27 14:16:40 +0200

    Changed some things

--- main.py
+++ main.py
@@ -39,6 +39,21 @@ while True:
 
     if process == None or process.poll() != None:
         log("STARTING THE SERVER")
+
+        ## Changing some files to defaults or specified by user
+        f = open("eula.txt", "w")
+        f.write("eula=true")
+        f.close()
+
+        os.system("cp -f ../banned* ./")
+        os.system("cp -f ../ops.json ./")
+        os.system("cp -f ../server-icon.png server-icon.png")
+        os.system("cp -f ../whitelist.json ./")
+
+        log("All needed files changed, added, edited")
+
+        
+
         process = subprocess.Popen( # Don't blame me for not having logs - Gemini AI wrote this lol
             variables.command,
             stdout=subprocess.PIPE,
@@ -47,21 +62,6 @@ while True:
         )
         log("PROCESS STARTED" + str(process))
 
-        ## Changing some files to defaults or specified by user
-        f = open("eula.txt", "w")
-        f.write("eula=true")
-        f.close()
-
-        default = open("../default.properties", "rt")
-        properties = open("server.properties", "w")
-        properties.write(default.read())
-
-        default.close()
-        properties.close()
-        log("EULA and server properties changed")
-
-        os.system("cp ../server-icon.png server-icon.png")
     else:
         log("Server is running well!")
-    log("WAITING 1m")
     time.sleep(60)
